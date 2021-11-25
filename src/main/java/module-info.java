705911960587d31dff8d9d33b6e0eb91fb22dd7a
PiDMA <PintoDMA@gmail.com>
2021-11-24 21:44:26 -0500

    Fixed redundant files

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.project.assignmenttwo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.project.assignmenttwo to javafx.fxml;
-    exports com.project.assignmenttwo;
-}
